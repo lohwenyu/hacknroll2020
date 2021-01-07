48479c1ba1e710434e21b30b7f2a9ed459f5251f
lohwenyu <e0421622@u.nus.edu>
2021-01-07 18:18:29 +0800

    home page brief skeleton

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { View, StyleSheet } from "react-native";
-import BookmarkedCourt from './src/components/BookmarkedCourt';
 import Bookmarks from './src/pages/Bookmarks';
-import LocationInformation from "./src/pages/LocationInformation";
-import Login from "./src/pages/LoginPage"
-import User from './src/pages/User';
 
 import database from "./src/databaseExample";
+import HomePage from './src/pages/HomePage';
 
 export default function App() {
     return (
         // <View style={styles.container}>
 		// <User user={database["Users"]["UserID2"]} known={true} teamedUp={true}/>
-		<Bookmarks user={database["Users"]["UserID2"]}/>
+		// <Bookmarks user={database["Users"]["UserID2"]}/>
+		<HomePage/>
         // </View>
     );
 }
