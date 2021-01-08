f7920f9165eebcda8b54f5a22329f1863c1c4cd2
lohwenyu <e0421622@u.nus.edu>
2021-01-08 19:40:58 +0800

    sign up db working

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -21,20 +21,29 @@ export default function SignupForm() {
             {/* Havent put error for email */}
             <InputBox name="Email" onChangeText={(email) => setEmail(email)}/>
             <InputBox name="Password" password={true} onChangeText={(password) => setPassword(password)}/>
-            <Button name="Sign Up" color="blue" onPress={() => signupUser(navigation, email, password)}/>
+            <Button name="Sign Up" color="blue" onPress={() => signupUser(navigation, email, password, username)}/>
         </View>
     )
 };
 
-function signupUser( navigation, email, password) {
+function signupUser( navigation, email, password, username) {
     try {
         if (password.length < 6) {
             alert("Please enter at least 6 characters.");
             return;
         }
-        firebase.auth().createUserWithEmailAndPassword(email, password).then((user) => {
-            console.log(user);
+        firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredentials) => {
+            console.log(userCredentials.user.uid);
             navigation.navigate("NavBar");
+            firebase.database().ref("/Users/" + userCredentials.user.uid).set({
+                "Name": username,
+                "GamesPlayed": 0,
+                "Won": 0,
+                "Lost": 0,
+                "AveragePPG": 0,
+                "DisplayPicture": require("../images/placeholder.png"),
+                "Bookmarks": []
+            })
         });
     } catch (error) {
         console.log(error.toString())
